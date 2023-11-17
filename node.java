a5d9be7e55721671a3fc341c3437854b9d4738d0
Abil Damirbek uulu <116529783+abildamirbekuulu@users.noreply.github.com>
2023-11-16 21:12:26 -0800

    Create node.java

---
+++ node.java
@@ -0,0 +1,38 @@
+
+public class Node {
+    // Declare instance variables
+    private Game game;
+    private Node left, right;
+
+    // Define constructor
+    public Node(Game game) {
+        this.game = game;
+        left = null;
+        right = null;
+    }
+
+    // Define accessors/mutators
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+}
